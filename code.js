c65519a82f7bafdbd6e5270690409ba5b2dd0e9d
cherkhnv <48092236+cherkhnv@users.noreply.github.com>
2019-06-14 16:02:00 +0700

    Забытые файлы

--- code.js
+++ code.js
@@ -23,13 +23,19 @@ const blogObserver = new EventObserver()
 const textField = document.querySelector('.textField')
 const countField = document.querySelector('.countField')
 
+
 const getWordsCount = text =>
   text ? text.trim().split(/\s+/).length : 0
+ 
+
 
 blogObserver.subscribe(text => {
   countField.innerHTML = getWordsCount(text)
 })
 
+
+
 textField.addEventListener('keyup', () => {
   blogObserver.broadcast(textField.value)
 })
+
